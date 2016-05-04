2c33974f00aa59f7470bf3f121821afa630ed0d4
Snake174 <jonnysereb@gmail.com>
2016-05-04 15:06:22 +0600

    SDK

--- tools/SDK.bat
+++ tools/SDK.bat
@@ -1,4 +1,6 @@
 @echo off
+set http_proxy=http://osintsev:qp3zxc@192.168.0.225:3128
+set https_proxy=http://osintsev:qp3zxc@192.168.0.225:3128
 echo "Downloading JDK 7u55 ..."
 wget.exe --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u55-b13/jdk-7u55-windows-x64.exe" -O ..\SDK\jdk-7u55-windows-x64.exe
 echo "Downloading Apache ANT ..."
@@ -21,5 +23,10 @@ echo "Installing Android SDK ..."
 %CD%\jdk-7u55\bin\jar.exe xfv android-sdk.zip
 del /Q android-sdk.zip
 set JAVA_HOME=%CD%\jdk-7u55
-set PATH=%PATH%;%CD%\..\tools;%JAVA_HOME%;%JAVA_HOME%\bin
-call %CD%\android-sdk-windows\tools\android.bat update sdk -u --all -t 1,2,19,29,38,94,95,108,112,113,125,145,149,150,151,152,153,154,156
+set ANT_HOME=%CD%\apache-ant-1.9.7
+set ANDROID_NDK=%CD%\android-ndk-r11c
+set ANDROID_SDK=%CD%\android-sdk-windows
+set ANDROID_HOME=%CD%\android-sdk-windows
+set ANDROID_SWT=%CD%\jdk-7u55
+set PATH=%PATH%;%CD%\..\tools;%JAVA_HOME%;%ANDROID_SDK\tools%;%ANDROID_NDK%;%ANT_HOME%
+%CD%\android-sdk-windows\tools\android.bat update sdk -u --all -t 1,2,19,29,38,94,95,108,112,113,125,145,149,150,151,152,153,154,156
